a105de9fe59b4f4dcff3f0c88642d60d5dc775ed
NATALIA E <NATALIA E@NATALIA>
2018-09-24 23:45:56 -0400

    resized grid

--- src/Worlds/WorldBase.java
+++ src/Worlds/WorldBase.java
@@ -53,7 +53,7 @@ public abstract class WorldBase {
 
     public void render(Graphics g){
 
-        for (int i = 0; i <= 800; i = i + GridPixelsize) {
+        for (int i = 0; i <= 780; i = i + GridPixelsize) {
 
             g.setColor(Color.BLACK);
             g.drawLine(0, i, handler.getWidth() , i);
