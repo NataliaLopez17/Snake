a84ce9c9d6cb0909193e6d7aa55bfd78da0fbd85
NATALIA E <NATALIA E@NATALIA>
2018-09-28 22:48:31 -0400

    added last minute audio clips for buttons

--- src/Game/GameStates/GameOver.java
+++ src/Game/GameStates/GameOver.java
@@ -7,6 +7,16 @@ import UI.UIImageButton;
 import UI.UIManager;
 
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * Created by AlexVR on 7/1/2018.
@@ -15,6 +25,13 @@ public class GameOver extends State {
 
     private int count = 0;
     private UIManager uiManager;
+
+    //Res.music
+    private InputStream audioFile;
+    private AudioInputStream audioStream;
+    private AudioFormat format;
+    private DataLine.Info info;
+    private Clip audioClip;
 
     public GameOver(Handler handler) {
         super(handler);
@@ -27,12 +44,46 @@ public class GameOver extends State {
                 handler.getMouseManager().setUimanager(null);
                 handler.getGame().reStart();
                 State.setState(handler.getGame().gameState);
+                
+                try {
+                    audioFile = getClass().getResourceAsStream("/music/Undertale-Sound-Effect-Saving.wav");
+                    audioStream = AudioSystem.getAudioInputStream(audioFile);
+                    format = audioStream.getFormat();
+                    info = new DataLine.Info(Clip.class, format);
+                    audioClip = (Clip) AudioSystem.getLine(info);
+                    audioClip.open(audioStream);
+                    audioClip.start();
+        		}
+        		catch (UnsupportedAudioFileException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                }
             } }));
 
 
         uiManager.addObjects(new UIImageButton(500, (300+(15+16))+(15+16), 150, 150, Images.titleButtonGameOver, () -> {
             handler.getMouseManager().setUimanager(null);
             State.setState(handler.getGame().menuState);
+            
+            try {
+                audioFile = getClass().getResourceAsStream("/music/Undertale-Sound-Effect-Asgore-Summoning-Weapon.wav");
+                audioStream = AudioSystem.getAudioInputStream(audioFile);
+                format = audioStream.getFormat();
+                info = new DataLine.Info(Clip.class, format);
+                audioClip = (Clip) AudioSystem.getLine(info);
+                audioClip.open(audioStream);
+                audioClip.start();
+    		}
+    		catch (UnsupportedAudioFileException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (LineUnavailableException e) {
+                e.printStackTrace();
+            }  
         }));
 
     }
--- src/Game/GameStates/PauseState.java
+++ src/Game/GameStates/PauseState.java
@@ -6,6 +6,16 @@ import UI.UIImageButton;
 import UI.UIManager;
 
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * Created by AlexVR on 7/1/2018.
@@ -15,6 +25,13 @@ public class PauseState extends State {
     private int count = 0;
     private UIManager uiManager;
 
+    //Res.music
+    private InputStream audioFile;
+    private AudioInputStream audioStream;
+    private AudioFormat format;
+    private DataLine.Info info;
+    private Clip audioClip;
+
     public PauseState(Handler handler) {
         super(handler);
         uiManager = new UIManager(handler);
@@ -23,11 +40,45 @@ public class PauseState extends State {
         uiManager.addObjects(new UIImageButton(220, 590, 150, 150, Images.Resume, () -> {
             handler.getMouseManager().setUimanager(null);
             State.setState(handler.getGame().gameState);
+            
+            try {
+                audioFile = getClass().getResourceAsStream("/music/Undertale-Sound-Effect-Battle-Encounter.wav");
+                audioStream = AudioSystem.getAudioInputStream(audioFile);
+                format = audioStream.getFormat();
+                info = new DataLine.Info(Clip.class, format);
+                audioClip = (Clip) AudioSystem.getLine(info);
+                audioClip.open(audioStream);
+                audioClip.start();
+    		}
+    		catch (UnsupportedAudioFileException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (LineUnavailableException e) {
+                e.printStackTrace();
+            }
         }));
 
         uiManager.addObjects(new UIImageButton(500, (380+(100+16))+(64+16), 150, 150, Images.BTitle, () -> {
             handler.getMouseManager().setUimanager(null);
             State.setState(handler.getGame().menuState);
+            
+            try {
+                audioFile = getClass().getResourceAsStream("/music/Undertale-Sound-Effect-Asgore-Summoning-Weapon.wav");
+                audioStream = AudioSystem.getAudioInputStream(audioFile);
+                format = audioStream.getFormat();
+                info = new DataLine.Info(Clip.class, format);
+                audioClip = (Clip) AudioSystem.getLine(info);
+                audioClip.open(audioStream);
+                audioClip.start();
+    		}
+    		catch (UnsupportedAudioFileException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (LineUnavailableException e) {
+                e.printStackTrace();
+            }
         }));
 
     }
